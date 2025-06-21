e8b3cbddc8dbfdedac0f525bbc3e9004031a8720
JosueIsOffline <josuehernandez2314@gmail.com>
2025-06-21 16:02:57 -0400

    feat: init api platform proyect

---
+++ homework-5/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use JosueIsOffline\Framework\Http\Kernel;
+use JosueIsOffline\Framework\Http\Request;
+use JosueIsOffline\Framework\View\ViewResolver;
+use JosueIsOffline\Framework\Routing\RouteLoader;
+
+
+define('BASE_PATH', dirname(__DIR__));
+
+require_once(BASE_PATH . '/vendor/autoload.php');
+
+$request = Request::create();
+$routeLoader = new RouteLoader();
+$kernel = new Kernel($routeLoader);
+
+$viewResolver = new ViewResolver();
+$viewResolver->addViewPath(BASE_PATH, '/views');
+
+$response = $kernel->handle($request);
+$response->send();
