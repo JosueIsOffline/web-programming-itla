d6954351e6f20132b77ed22a0b7f0b69b1a97bf9
JosueIsOffline <josuehernandez2314@gmail.com>
2025-07-03 01:30:57 -0400

    chore: just testing thing

--- homework-5/public/index.php
+++ homework-5/public/index.php
@@ -4,6 +4,7 @@ use JosueIsOffline\Framework\Http\Kernel;
 use JosueIsOffline\Framework\Http\Request;
 use JosueIsOffline\Framework\View\ViewResolver;
 use JosueIsOffline\Framework\Routing\RouteLoader;
+use JosueIsOffline\Framework\FrameworkBootstrap;
 
 define('BASE_PATH', dirname(__DIR__));
 
@@ -12,12 +13,24 @@ require_once(BASE_PATH . '/vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
+FrameworkBootstrap::boot();
+
 $request = Request::create();
+
+$redirectPath = FrameworkBootstrap::handleSetupRedirect($request);
+if ($redirectPath) {
+  header("Location: $redirectPath");
+  exit;
+}
+
 $routeLoader = new RouteLoader();
+
+FrameworkBootstrap::registerSystemRoutes($routeLoader);
+
 $kernel = new Kernel($routeLoader);
 
 $viewResolver = new ViewResolver();
-$viewResolver->addViewPath(BASE_PATH, '/views');
+$viewResolver->addViewPath(BASE_PATH . '/views');
 
 $response = $kernel->handle($request);
 $response->send();
